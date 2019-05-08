632e5dd6cc1ed71095aceb86948218f53b70f226
Nino Lam <Nino@MacBook-Pro-de-Moi-2.local>
2019-05-09 01:42:11 +0200

    add resize div

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App  = () => { 
+  
+  function changevalue(e) {
+    e.preventDefault();
+    var resizer = document.querySelector('.resizer');
+
+    document.addEventListener('mousemove', resizeX);
+    document.addEventListener('mouseup', function (e) {
+      document.removeEventListener('mousemove', resizeX);
+      document.body.style.cursor = 'auto';
+    });
+  
+    function resizeX(e) {
+      var bodyWidth = 100 / document.body.clientWidth;
+      var box1 = document.querySelector('.PlayerA');
+      var box2 = document.querySelector('.PlayerB');
+      var leftHeight = (parseFloat(window.getComputedStyle(box1, '').width) + e.movementX) * bodyWidth;
+      box1.style.width = leftHeight + '%';
+      box2.style.width = 100 - leftHeight + '%';
+  
+      if (box1.style.width >= "70%" && box1.style.width <= "71%") {
+        box1.style.width = leftHeight + 20 + '%';
+        ;
+      } 
+      else if (box1.style.width >= "84%" && box1.style.width <= "85%") {
+        box1.style.width = '50%';
+      }
+
+      if (box1.style.width >= "30%" && box1.style.width <= "31%") {
+        box1.style.width = leftHeight - 20 + '%';
+        ;
+      } 
+      else if (box1.style.width >= "14%" && box1.style.width <= "15%") {
+        box1.style.width = '50%';
+      }
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="Players-container">
+
+    <div className="content-box PlayerA left">
+      <div className="header">
+        Player 1
+      </div>
+      <div></div>
+      <div onMouseDown={changevalue} className="resizer"></div>
     </div>
+
+
+    <div className="content-box PlayerB right">
+      <div className="header">
+        Player 2
+      </div>
+      <div></div>
+    </div>
+
+
+  </div>
   );
 }
 
