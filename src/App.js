0cbf664ece745879e83ff1dbbd29202710a91011
Nino Lam <Nino@MacBook-Pro-de-Moi-2.local>
2019-05-09 17:22:35 +0200

    comparaison-page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import resize from './imgs/resize.png';
 
 const App  = () => { 
   
@@ -17,24 +18,31 @@ const App  = () => {
       var bodyWidth = 100 / document.body.clientWidth;
       var box1 = document.querySelector('.PlayerA');
       var box2 = document.querySelector('.PlayerB');
+      var PlayerContainerA = document.querySelector('.PlayerA .Player-container');
+      var PlayerContainerB = document.querySelector('.PlayerB .Player-container');
       var leftHeight = (parseFloat(window.getComputedStyle(box1, '').width) + e.movementX) * bodyWidth;
       box1.style.width = leftHeight + '%';
       box2.style.width = 100 - leftHeight + '%';
   
       if (box1.style.width >= "70%" && box1.style.width <= "71%") {
-        box1.style.width = leftHeight + 20 + '%';
-        ;
+        box1.style.width = leftHeight + 25 + '%';
+        PlayerContainerB.style.visibility = "hidden"
       } 
       else if (box1.style.width >= "84%" && box1.style.width <= "85%") {
         box1.style.width = '50%';
+        PlayerContainerB.style.visibility = "visible"
+      }
+      else{
+
       }
 
       if (box1.style.width >= "30%" && box1.style.width <= "31%") {
-        box1.style.width = leftHeight - 20 + '%';
-        ;
+        box1.style.width = leftHeight - 30 + '%';
+        PlayerContainerA.style.visibility = "hidden"
       } 
       else if (box1.style.width >= "14%" && box1.style.width <= "15%") {
         box1.style.width = '50%';
+        PlayerContainerA.style.visibility = "visible"
       }
     }
   };
@@ -42,19 +50,41 @@ const App  = () => {
   <div className="Players-container">
 
     <div className="content-box PlayerA left">
-      <div className="header">
-        Player 1
+      <div className="Player-container">
+        <h2 className="Player-title">
+          Joueur 1
+        </h2>
+        <p className="description">Choississez le joueur ou l’équipe que vous souhaitez comparez, si vous souhaitez seulement analyser un joueur glissez le rond vers la droite.</p>
+        <div className="Search-container">
+          <input type="text" placeholder="Recherche ..." />
+          <select>
+              <option value="2018-2019">2018-2019</option>
+              <option value="2017-2018">2017-2018</option>
+              <option value="2016-2017">2016-2017</option>
+              <option value="2015-2016">2015-2016</option>
+          </select>
+          </div>
       </div>
-      <div></div>
-      <div onMouseDown={changevalue} className="resizer"></div>
+      <div onMouseDown={changevalue} className="resizer"><span className="helper"></span><img src={resize} /> </div>
     </div>
 
 
     <div className="content-box PlayerB right">
-      <div className="header">
-        Player 2
+    <div className="Player-container">
+        <h2 className="Player-title">
+          Joueur 2
+        </h2>
+        <p className="description">Choississez le joueur ou l’équipe que vous souhaitez comparez, si vous souhaitez seulement analyser un joueur glissez le rond vers la droite.</p>
+        <div className="Search-container">
+          <input type="text" placeholder="Recherche ..." />
+          <select>
+              <option value="2018-2019">2018-2019</option>
+              <option value="2017-2018">2017-2018</option>
+              <option value="2016-2017">2016-2017</option>
+              <option value="2015-2016">2015-2016</option>
+          </select>
+        </div>
       </div>
-      <div></div>
     </div>
 
 
